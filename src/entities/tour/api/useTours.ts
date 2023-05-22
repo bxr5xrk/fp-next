71b469c2f9068eef28dc456ffa666d6acbf83438
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-05-22 21:01:36 +0300

    fix

--- src/entities/tour/api/useTours.ts
+++ src/entities/tour/api/useTours.ts
@@ -1,16 +1,16 @@
 import { Tour } from '../types/tour';
-import db from '../../../db.json';
+// import db from '../../../db.json';
 
 export const useTours = async (): Promise<Tour[]> => {
-  // const res = await fetch(process.env.NEXT_PUBLIC_API_URL + 'api/tours');
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + 'api/tours');
 
-  // return res.json();
-  return db.tours;
+  return res.json();
+  // return db.tours;
 };
 
 export const useTourById = async (id: string): Promise<Tour> => {
-  // const res = await fetch(process.env.NEXT_PUBLIC_API_URL + 'api/tours/' + id);
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + 'api/tours/' + id);
 
-  // return res.json();
-  return db.tours[0];
+  return res.json();
+  // return db.tours[0];
 };
