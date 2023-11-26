a98e1c1891969a2f7fde34fc548956ee6875c20c
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-11-26 22:51:17 +0200

    fix

--- src/widgets/calendar/lib/get-calendar-data.ts
+++ src/widgets/calendar/lib/get-calendar-data.ts
@@ -2,7 +2,7 @@ import { Calendar, Tour } from "../types";
 import { groupEventsByMonth } from "./group-events-by-month";
 
 export const getCalendarData = (tours: Tour[]): Calendar => {
-  const sortedTours = tours.toSorted((a, b) => b.startDate.localeCompare(a.startDate));
+  const sortedTours = [...tours].sort((a, b) => b.startDate.localeCompare(a.startDate));
 
   return groupEventsByMonth(
     sortedTours
