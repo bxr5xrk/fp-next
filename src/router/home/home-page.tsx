7eb6521c24ccbe26b2ca51812667be962dc70446
palyanytsya <vasyldobrovoslkyi@gmail.com>
2024-01-27 17:34:31 +0200

    feat[root]: removed all content

--- src/router/home/home-page.tsx
+++ src/router/home/home-page.tsx
@@ -1,13 +1,13 @@
-import { Tours } from "@/entities/tour";
-import { Categories } from "@/features/Categories";
-import { Sorting } from "@/features/Sorting";
-import { Calendar } from "@/widgets/calendar";
-import { Suspense } from "react";
+// import { Tours } from "@/entities/tour";
+// import { Categories } from "@/features/Categories";
+// import { Sorting } from "@/features/Sorting";
+// import { Calendar } from "@/widgets/calendar";
+// import { Suspense } from "react";
 
 export function HomePage() {
   return (
     <section className="flex">
-      <Calendar />
+      {/* <Calendar />
 
       <div className="ms:py-0 w-full space-y-4">
         <div className="flex flex-col items-start justify-between gap-4 lg:flex-row lg:items-center">
@@ -22,7 +22,7 @@ export function HomePage() {
         <Suspense>
           <Tours />
         </Suspense>
-      </div>
+      </div> */}
     </section>
   );
 };
